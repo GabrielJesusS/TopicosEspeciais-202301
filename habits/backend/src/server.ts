b8bcfa8ef4daaeeb40ce4b4bdd52962ba2e5c152
Gabriel Jesus <78988957+GabrielJesusS@users.noreply.github.com>
2023-02-27 11:44:54 -0300

    build: project structure

---
+++ habits/backend/src/server.ts
@@ -0,0 +1,29 @@
+import Fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
+import cors from "@fastify/cors";
+
+const app = Fastify();
+const prisma = new PrismaClient();
+
+app.get("/hello", () => {
+  return "Hello World";
+});
+
+app.get("/hello2", async () => {
+  const habits = await prisma.habit.findMany({
+    where: {
+      title: {
+        startsWith: "Beber",
+      },
+    },
+  });
+  return habits
+});
+
+app
+  .listen({
+    port: 3000,
+  })
+  .then(() => {
+    console.log("HTTP server running");
+  });
